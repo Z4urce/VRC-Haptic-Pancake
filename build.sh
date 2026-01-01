664aac6643e02bb774b8afda7409e0aaec1366da
digitalfox <git@vulpine.engineer>
2025-12-31 22:19:50 -0500

    [Add] Do clean build on Linux with ./build.sh
    On Linux, modify "build.sh" to use a clean Python virtual environment
    to build Haptic Pancake.
    
    TODO: Update the Windows build.bat, migrate GitHub Actions to use
    scripts instead.

--- build.sh
+++ build.sh
@@ -1,5 +1,31 @@
-#!/bin/sh
+#!/bin/bash
+# See http://redsymbol.net/articles/unofficial-bash-strict-mode/
+set -euo pipefail
+
 # Build Haptic Pancake bridge app on Linux
 #
 # NOTE: If changing this, make sure "build.bat" is also updated!
-pyinstaller -wF --collect-all openvr --name hapticpancake --icon=Images\icon.ico BridgeApp/main.py
+
+# Build and activate a Python virtual environment to keep your system clean
+if [ ! -d "ignored-hpb-venv" ]; then
+    python3 -m venv "ignored-hpb-venv"
+fi
+source "ignored-hpb-venv/bin/activate"
+
+# Install/update Haptic Pancake dependencies
+pip3 install -r "BridgeApp/requirements.txt"
+
+# Add PyInstaller
+pip3 install pyinstaller
+
+pyinstaller -wF --collect-all openvr --name hapticpancake_linux --icon=Images\icon.ico BridgeApp/main.py
+# "hapticpancake.spec"
+
+# Spec files don't need to stick around if you're generating them with a script
+# and not modifying the result
+rm "hapticpancake_linux.spec"
+
+echo
+echo "--------------------------------"
+echo "Result: dist/hapticpancake_linux"
+echo "--------------------------------"
